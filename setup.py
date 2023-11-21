637b9a4fa5458e5355c4e280d5df03cf82545161
NteRySin <88823141+NteRySin@users.noreply.github.com>
2023-11-21 17:36:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="streaminguploadserver",
-    version="0.0.2",
+    version="0.0.3",
     author="NteRySin",
     author_email="author@example.com",
     description="Streaming upload server in Python extended from http.server",
